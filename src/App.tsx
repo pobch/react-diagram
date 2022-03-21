1817d18235130c3b27488b88ded72982030f4b19
Pob <590650@gmail.com>
2022-03-21 20:22:19 +0700

    Add moving feature

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ import { Drawable } from 'roughjs/bin/core'
 const generator = rough.generator()
 
 type TElementData = {
-  id: number | string
+  id: number
   x1: number
   y1: number
   x2: number
   y2: number
-  type: 'selection' | 'line' | 'rectangle'
+  type: 'line' | 'rectangle'
   roughElement: Drawable
 }
 
@@ -50,6 +50,50 @@ function createRectangleElement({
   }
 }
 
+type TElementWithPointerOffsetData = TElementData & {
+  pointerOffsetX1: number
+  pointerOffsetY1: number
+}
+
+function getFirstElementAtPosition({
+  dataSource,
+  xPosition,
+  yPosition,
+}: {
+  dataSource: TElementData[]
+  xPosition: number
+  yPosition: number
+}): TElementWithPointerOffsetData | undefined {
+  function distance(a: { x: number; y: number }, b: { x: number; y: number }) {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
+  }
+  const foundElement = dataSource.find((element) => {
+    if (element.type === 'line') {
+      const a = { x: element.x1, y: element.y1 }
+      const b = { x: element.x2, y: element.y2 }
+      const c = { x: xPosition, y: yPosition }
+      const distanceOffset = distance(a, b) - (distance(a, c) + distance(b, c))
+      return Math.abs(distanceOffset) < 1
+    } else if (element.type === 'rectangle') {
+      const minX = Math.min(element.x1, element.x2)
+      const maxX = Math.max(element.x1, element.x2)
+      const minY = Math.min(element.y1, element.y2)
+      const maxY = Math.max(element.y1, element.y2)
+      return minX <= xPosition && xPosition <= maxX && minY <= yPosition && yPosition <= maxY
+    }
+    // should not reach here
+    return false
+  })
+
+  if (!foundElement) return
+
+  return {
+    ...foundElement,
+    pointerOffsetX1: xPosition - foundElement.x1,
+    pointerOffsetY1: yPosition - foundElement.y1,
+  }
+}
+
 /**
  * * ------------------------------
  * *          Component
@@ -58,11 +102,27 @@ function createRectangleElement({
 export function App() {
   const [tool, setTool] = useState<'selection' | 'line' | 'rectangle'>('selection')
   const [elements, setElements] = useState<TElementData[]>([])
-  const [action, setAction] = useState<'none' | 'drawing'>('none')
+  const [action, setAction] = useState<'none' | 'drawing' | 'moving'>('none')
+  const [selectedElementWithPointerOffset, setSelectedElementWithPointerOffset] = useState<
+    TElementWithPointerOffsetData | undefined
+  >()
+  // TODO: implement this
+  const [cursorType, setCursorType] = useState('default')
 
   function handlePointerDown(e: React.PointerEvent) {
     const { clientX, clientY } = e
-    if (tool === 'line') {
+
+    if (tool === 'selection') {
+      const selectedElemData = getFirstElementAtPosition({
+        dataSource: elements,
+        xPosition: clientX,
+        yPosition: clientY,
+      })
+      if (!selectedElemData) return
+      setAction('moving')
+      setSelectedElementWithPointerOffset(selectedElemData)
+      return
+    } else if (tool === 'line') {
       const nextIndex = elements.length
       const newElement = createLineElement({
         id: nextIndex,
@@ -73,6 +133,7 @@ export function App() {
       })
       setElements((prevState) => [...prevState, newElement])
       setAction('drawing')
+      return
     } else if (tool === 'rectangle') {
       const nextIndex = elements.length
       const newElement = createRectangleElement({
@@ -84,12 +145,45 @@ export function App() {
       })
       setElements((prevState) => [...prevState, newElement])
       setAction('drawing')
+      return
     }
   }
 
   function handlePointerMove(e: React.PointerEvent) {
     const { clientX, clientY } = e
-    if (action === 'drawing') {
+    if (action === 'moving' && selectedElementWithPointerOffset) {
+      const newX1 = clientX - selectedElementWithPointerOffset.pointerOffsetX1
+      const newY1 = clientY - selectedElementWithPointerOffset.pointerOffsetY1
+      // replace specific element
+      const index = selectedElementWithPointerOffset.id
+      const elementsCopy = [...elements]
+
+      if (tool === 'selection' && selectedElementWithPointerOffset.type === 'line') {
+        const distanceX = selectedElementWithPointerOffset.x2 - selectedElementWithPointerOffset.x1
+        const distanceY = selectedElementWithPointerOffset.y2 - selectedElementWithPointerOffset.y1
+        const newElement = createLineElement({
+          id: index,
+          x1: newX1,
+          y1: newY1,
+          x2: newX1 + distanceX,
+          y2: newY1 + distanceY,
+        })
+        elementsCopy[index] = newElement
+      } else if (tool === 'selection' && selectedElementWithPointerOffset.type === 'rectangle') {
+        const width = selectedElementWithPointerOffset.x2 - selectedElementWithPointerOffset.x1
+        const height = selectedElementWithPointerOffset.y2 - selectedElementWithPointerOffset.y1
+        const newElement = createRectangleElement({
+          id: index,
+          x1: newX1,
+          y1: newY1,
+          width: width,
+          height: height,
+        })
+        elementsCopy[index] = newElement
+      }
+      setElements(elementsCopy)
+      return
+    } else if (action === 'drawing') {
       // replace last element
       const lastIndex = elements.length - 1
       const { x1: currentX1, y1: currentY1 } = elements[lastIndex]
@@ -115,11 +209,14 @@ export function App() {
         elementsCopy[lastIndex] = newElement
       }
       setElements(elementsCopy)
+      return
     }
   }
 
   function handlePointerUp(e: React.PointerEvent) {
     setAction('none')
+    setSelectedElementWithPointerOffset(undefined)
+    return
   }
 
   // * ----------- Clear Canvas -------------
