50a4e8a53deed4473d0b618117e5750aeb8d9a3c
Pob <590650@gmail.com>
2022-03-21 17:29:49 +0700

    Refactor + Extract helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,92 @@ import { useLayoutEffect, useRef, useState } from 'react'
 import rough from 'roughjs/bundled/rough.esm'
 import { Drawable } from 'roughjs/bin/core'
 
+/**
+ * * ------------------------------
+ * *          Helpers
+ * * ------------------------------
+ */
+
 const generator = rough.generator()
 
+type TElementData = {
+  id: number | string
+  x1: number
+  y1: number
+  x2: number
+  y2: number
+  type: 'selection' | 'line' | 'rectangle'
+  roughElement: Drawable
+}
+
+function createLineElement({
+  id,
+  x1,
+  y1,
+  x2,
+  y2,
+}: Omit<TElementData, 'roughElement' | 'type'>): TElementData {
+  const roughElement = generator.line(x1, y1, x2, y2)
+  return { id: id, x1: x1, y1: y1, x2: x2, y2: y2, type: 'line', roughElement }
+}
+
+function createRectangleElement({
+  id,
+  x1,
+  y1,
+  width,
+  height,
+}: Pick<TElementData, 'id' | 'x1' | 'y1'> & { width: number; height: number }): TElementData {
+  const roughElement = generator.rectangle(x1, y1, width, height)
+  return {
+    id: id,
+    x1: x1,
+    y1: y1,
+    x2: x1 + width,
+    y2: y1 + height,
+    type: 'rectangle',
+    roughElement,
+  }
+}
+
+/**
+ * * ------------------------------
+ * *          Component
+ * * ------------------------------
+ */
 export function App() {
   const [tool, setTool] = useState<'selection' | 'line' | 'rectangle'>('selection')
-  const [elements, setElements] = useState<
-    {
-      id: number | string
-      x1: number
-      y1: number
-      x2: number
-      y2: number
-      type: 'selection' | 'line' | 'rectangle'
-      roughElement: Drawable
-    }[]
-  >([])
+  const [elements, setElements] = useState<TElementData[]>([])
   const [action, setAction] = useState<'none' | 'drawing'>('none')
 
-  function handleMouseDown(e: React.MouseEvent) {
+  function handlePointerDown(e: React.PointerEvent) {
     const { clientX, clientY } = e
     if (tool === 'line') {
-      const id = elements.length
-      const roughElement = generator.line(clientX, clientY, clientX, clientY)
-      const newElement = {
-        id: id,
+      const nextIndex = elements.length
+      const newElement = createLineElement({
+        id: nextIndex,
         x1: clientX,
         y1: clientY,
         x2: clientX,
         y2: clientY,
-        type: tool,
-        roughElement,
-      }
+      })
       setElements((prevState) => [...prevState, newElement])
       setAction('drawing')
     } else if (tool === 'rectangle') {
-      const id = elements.length
-      const roughElement = generator.rectangle(clientX, clientY, 0, 0)
-      const newElement = {
-        id: id,
+      const nextIndex = elements.length
+      const newElement = createRectangleElement({
+        id: nextIndex,
         x1: clientX,
         y1: clientY,
-        x2: clientX,
-        y2: clientY,
-        type: tool,
-        roughElement,
-      }
+        width: 0,
+        height: 0,
+      })
       setElements((prevState) => [...prevState, newElement])
       setAction('drawing')
     }
   }
 
-  function handleMouseMove(e: React.MouseEvent) {
+  function handlePointerMove(e: React.PointerEvent) {
     const { clientX, clientY } = e
     if (action === 'drawing') {
       // replace last element
@@ -61,40 +96,29 @@ export function App() {
       const elementsCopy = [...elements]
 
       if (tool === 'line') {
-        const roughElement = generator.line(currentX1, currentY1, clientX, clientY)
-        const newElement = {
+        const newElement = createLineElement({
           id: lastIndex,
           x1: currentX1,
           y1: currentY1,
           x2: clientX,
           y2: clientY,
-          type: tool,
-          roughElement,
-        }
+        })
         elementsCopy[lastIndex] = newElement
       } else if (tool === 'rectangle') {
-        const roughElement = generator.rectangle(
-          currentX1,
-          currentY1,
-          clientX - currentX1,
-          clientY - currentY1
-        )
-        const newElement = {
+        const newElement = createRectangleElement({
           id: lastIndex,
           x1: currentX1,
           y1: currentY1,
-          x2: clientX,
-          y2: clientY,
-          type: tool,
-          roughElement,
-        }
+          width: clientX - currentX1,
+          height: clientY - currentY1,
+        })
         elementsCopy[lastIndex] = newElement
       }
       setElements(elementsCopy)
     }
   }
 
-  function handleMouseUp(e: React.MouseEvent) {
+  function handlePointerUp(e: React.PointerEvent) {
     setAction('none')
   }
 
@@ -174,10 +198,12 @@ export function App() {
           display: 'block',
           width: window.innerWidth,
           height: window.innerHeight,
+          // disable all touch behavior from browser, e.g. touch to scroll
+          touchAction: 'none',
         }}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
       >
         My Canvas
       </canvas>
