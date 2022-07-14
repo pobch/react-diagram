3aa06e5bbf38dfe4c511403fd1d8f9c37bf6f6f0
Pob <590650@gmail.com>
2022-07-14 16:35:44 +0700

    Implement ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
+import { ErrorBoundary } from './ErrorBoundary'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
